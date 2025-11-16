342b11157aac9fa35712361d9af2f750b1b78cb0
Rebecka Larsson <rebecka.rl.larsson@gmail.com>
2025-11-16 16:59:34 +0100

    Responsive navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import { Link, NavLink } from 'react-router'
 import logo_navbar from '../assets/logo_navbar.svg'
 import logo_door from '../assets/logo_door.svg'
-import { IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
   return (
@@ -16,35 +15,29 @@ const Navbar = () => {
                     </Link>
                 </div>
                 
-                {/* <div className='flex justify-between'> */}
-                    
-                    {/* Snappy Sliding Hamburger Menu from https://alvarotrigo.com/blog/hamburger-menu-css/ */}
+                {/* Snappy Sliding Hamburger Menu from https://alvarotrigo.com/blog/hamburger-menu-css/ */}
+                <div className="hamburger-menu">
+                    <input id="menu__toggle" type="checkbox" />
+                    <label className="menu__btn" htmlFor="menu__toggle">
+                    <span></span>
+                    </label>
+                    <ul className='menu__box nav-links container mx-auto px-4 flex flex-col gap-(--spacing-md)'>
+                        <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/">Home</NavLink></li>
+                        <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/stays">All castles</NavLink></li>
+                        <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/bookings">My bookings</NavLink></li>
+                        <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/register">Login / Sign Up</NavLink></li>
+                    </ul>
+                </div>
 
-                    <div className="hamburger-menu">
-                        <input id="menu__toggle" type="checkbox" />
-                        <label className="menu__btn" htmlFor="menu__toggle">
-                        <span></span>
-                        </label>
-                        {/* <IoMenu /> */}
-
-                        <ul className='menu__box nav-links container mx-auto px-4 flex flex-col gap-(--spacing-md)'>
-                            <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/">Home</NavLink></li>
-                            <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/stays">All castles</NavLink></li>
-                            <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/bookings">My bookings</NavLink></li>
-                            <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/register">Login / Sign Up</NavLink></li>
-                        </ul>
-                    </div>
-
-                    {/* LAPTOP NAV-LINKS */}
-                    <div className='nav-links laptop-nav pb-6'>
-                        <ul className='flex'>
-                            <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/">Home</NavLink></li>
-                            <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/stays">All castles</NavLink></li>
-                            <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/bookings">My bookings</NavLink></li>
-                            <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/register">Login / Sign Up</NavLink></li>
-                        </ul>
-                        </div>
-                {/* </div> */}
+                {/* LAPTOP NAV-LINKS */}
+                <div className='nav-links laptop-nav pb-6'>
+                    <ul className='flex'>
+                        <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/">Home</NavLink></li>
+                        <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/stays">All castles</NavLink></li>
+                        <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/bookings">My bookings</NavLink></li>
+                        <li><NavLink className="menu__item [&.active]:text-(--primary-purple)" to="/register">Login / Sign Up</NavLink></li>
+                    </ul>
+                </div>
             </nav>
         </div>
   )
