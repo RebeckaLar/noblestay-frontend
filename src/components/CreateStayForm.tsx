2109b238d35134b4d249fa83cb5086953ff42213
Rebecka Larsson <rebecka.rl.larsson@gmail.com>
2025-11-20 16:54:35 +0100

    CreateStayForm, new version, continuation

--- src/components/CreateStayForm.tsx
+++ src/components/CreateStayForm.tsx
@@ -1,6 +1,31 @@
+import { useStay } from "@/contexts/StayContext"
+import { useForm, type SubmitHandler } from "react-hook-form";
+
 const CreateStayForm = () => {
+
+    const { stays } = useStay()
+
+    const {
+      register,
+      setValue,
+      getValues,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<Stay>({
+    });
+
+    const onSubmit: SubmitHandler<Stay> = (data) => {
+        console.log("Create stay form submitted")
+    }
+
+
   return (
-    <div>CreateStayForm</div>
+    <div>CreateStayForm
+        //axios anrop för skapa listing
+        //sen svaret
+        //lägger in i creatlistingunction från StayContext
+        //i context funk satte /uppdatera jagalla mina
+    </div>
   )
 }
 export default CreateStayForm
